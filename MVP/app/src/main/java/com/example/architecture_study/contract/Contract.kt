a66e70b7265f370965e0321c7a5f5eb3a1e0a2d7
DevHyeonseong <ykiw911@naver.com>
2020-09-04 15:33:50 +0900

    MVP : 네이버 로그인

--- MVP/app/src/main/java/com/example/architecture_study/contract/Contract.kt
+++ MVP/app/src/main/java/com/example/architecture_study/contract/Contract.kt
@@ -1,7 +1,6 @@
 package com.example.architecture_study.contract
 
 import android.content.Context
-import com.example.architecture_study.retrofit.data.User
 import com.example.architecture_study.presenter.BasePresenter
 import com.example.architecture_study.view.BaseView
 import com.nhn.android.naverlogin.OAuthLogin
@@ -10,9 +9,12 @@ interface Contract {
 
     interface LoginView: BaseView {
         fun showUser(text: String)
+        fun getContext(context: Context)
     }
 
     interface LoginPresenter: BasePresenter<LoginView> {
         fun getUser(mOAuthLoginModule: OAuthLogin, context: Context)
     }
+
+
 }
